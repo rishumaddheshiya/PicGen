f67aef870ac680fae017dbf05004506209b80984
Rishu Maddheshiya <110919927+rishumaddheshiya@users.noreply.github.com>
2025-03-06 12:16:35 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [credit, setCredit] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://picgen-backend.onrender.com";
   const navigate = useNavigate()
 
   const loadCreditsData = async () => {
